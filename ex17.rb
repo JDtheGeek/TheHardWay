68c73d6cb8a8fac82d0977c9b0739dcc73078e09
JDtheGeek <john.dennehy@gmail.com>
2019-08-14 14:27:00 +0100

    Bug fix

--- ex17.rb
+++ ex17.rb
@@ -3,10 +3,10 @@ from_file, to_file = ARGV
 puts "Copying from #{from_file} to #{to_file}"
 
 # we could do these two on one line, how?
-# in_file = open(from_file)
-# indata = in_file.read
+in_file = open(from_file)
+indata = in_file.read
 
-indata = open(from_file).read
+# indata = open(from_file).read
 
 puts "The input file is #{indata.length} bytes long"
 
